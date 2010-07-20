494a90c08ad64dd3550e6d33ce22e48f53136a9f
Wes Garner <wesgarner@gmail.com>
2010-07-20 18:41:32 -0500

    update full_galaxys.mk

--- full_galaxys.mk
+++ full_galaxys.mk
@@ -2,8 +2,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := i9000
-PRODUCT_DEVICE := aries
+PRODUCT_NAME := Galaxy S
+PRODUCT_DEVICE := SGH-T959
 PRODUCT_MODEL := SGH-T959
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
@@ -12,18 +12,17 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 
 PRODUCT_COPY_FILES += \
-    device/samsung/i9000/init.smdkc110.rc:root/init.smdkc110.rc
+    device/samsung/galaxys/init.smdkc110.rc:root/init.smdkc110.rc
 
 PRODUCT_POLICY := android.policy_phone
 
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240 \
-    rild.libpath=/system/lib/libsec-ril.so \
+    rild.libpath=/system/lib/libreference-ril.so \
     rild.libargs=-d /dev/ttyS0 \
     wifi.interface=eth0 \
     wifi.supplicant_scan_interval=15
-
 
 
 
@@ -39,24 +38,24 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
 
 #
-# Copy i9000 specific prebuilt files
+# Copy galaxys specific prebuilt files
 #
 
 #
 # Wifi
 #
 PRODUCT_COPY_FILES += \
-    device/samsung/i9000/prebuilt/wifi/libwlservice.so:system/lib/libwlservice.so \
-    device/samsung/i9000/prebuilt/wifi/nvram_net.txt:system/etc/wifi/nvram_net.txt \
-    device/samsung/i9000/prebuilt/wifi/nvram_mfg.txt:system/etc/wifi/nvram_mfg.txt \
-    device/samsung/i9000/prebuilt/wifi/bcm4329_aps.bin:system/etc/wifi/bcm4329_aps.bin \
-    device/samsung/i9000/prebuilt/wifi/bcm4329_mfg.bin:system/etc/wifi/bcm4329_mfg.bin \
-    device/samsung/i9000/prebuilt/wifi/bcm4329_sta.bin:system/etc/wifi/bcm4329_sta.bin \
-    device/samsung/i9000/prebuilt/wifi/wifi.conf:system/etc/wifi/wifi.conf \
-    device/samsung/i9000/prebuilt/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-    device/samsung/i9000/prebuilt/wifi/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
-    device/samsung/i9000/prebuilt/wifi/wlservice:system/bin/wlservice \
-    device/samsung/i9000/prebuilt/wifi/wpa_supplicant:system/bin/wpa_supplicant
+    device/samsung/galaxys/prebuilt/wifi/libwlservice.so:system/lib/libwlservice.so \
+    device/samsung/galaxys/prebuilt/wifi/nvram_net.txt:system/etc/wifi/nvram_net.txt \
+    device/samsung/galaxys/prebuilt/wifi/nvram_mfg.txt:system/etc/wifi/nvram_mfg.txt \
+    device/samsung/galaxys/prebuilt/wifi/bcm4329_aps.bin:system/etc/wifi/bcm4329_aps.bin \
+    device/samsung/galaxys/prebuilt/wifi/bcm4329_mfg.bin:system/etc/wifi/bcm4329_mfg.bin \
+    device/samsung/galaxys/prebuilt/wifi/bcm4329_sta.bin:system/etc/wifi/bcm4329_sta.bin \
+    device/samsung/galaxys/prebuilt/wifi/wifi.conf:system/etc/wifi/wifi.conf \
+    device/samsung/galaxys/prebuilt/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    device/samsung/galaxys/prebuilt/wifi/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+    device/samsung/galaxys/prebuilt/wifi/wlservice:system/bin/wlservice \
+    device/samsung/galaxys/prebuilt/wifi/wpa_supplicant:system/bin/wpa_supplicant
 
 
 
@@ -64,58 +63,58 @@ PRODUCT_COPY_FILES += \
 # Display (3D)
 #
 PRODUCT_COPY_FILES += \
-    device/samsung/i9000/prebuilt/egl/egl.cfg:system/lib/egl/egl.cfg \
-    device/samsung/i9000/prebuilt/egl/libEGL_POWERVR_SGX540_120.so:system/lib/egl/libEGL_POWERVR_SGX540_120.so \
-    device/samsung/i9000/prebuilt/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
-    device/samsung/i9000/prebuilt/egl/libGLESv1_CM_POWERVR_SGX540_120.so:system/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
-    device/samsung/i9000/prebuilt/egl/libGLESv2_POWERVR_SGX540_120.so:system/lib/egl/libGLESv2_POWERVR_SGX540_120.so
+    device/samsung/galaxys/prebuilt/egl/egl.cfg:system/lib/egl/egl.cfg \
+    device/samsung/galaxys/prebuilt/egl/libEGL_POWERVR_SGX540_120.so:system/lib/egl/libEGL_POWERVR_SGX540_120.so \
+    device/samsung/galaxys/prebuilt/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
+    device/samsung/galaxys/prebuilt/egl/libGLESv1_CM_POWERVR_SGX540_120.so:system/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
+    device/samsung/galaxys/prebuilt/egl/libGLESv2_POWERVR_SGX540_120.so:system/lib/egl/libGLESv2_POWERVR_SGX540_120.so
 
 #
 # Sensors, Lights etc
 #
 PRODUCT_COPY_FILES += \
-    device/samsung/i9000/prebuilt/hw/copybit.s5pc110.so:system/lib/hw/copybit.s5pc110.so \
-    device/samsung/i9000/prebuilt/hw/lights.default.so:system/lib/hw/lights.default.so \
-    device/samsung/i9000/prebuilt/hw/sensors.SGH-T959.so:system/lib/hw/sensors.SGH-T959.so \
-    device/samsung/i9000/prebuilt/hw/gralloc.s5pc110.so:system/lib/hw/gralloc.s5pc110.so
+    device/samsung/galaxys/prebuilt/hw/copybit.s5pc110.so:system/lib/hw/copybit.s5pc110.so \
+    device/samsung/galaxys/prebuilt/hw/lights.default.so:system/lib/hw/lights.default.so \
+    device/samsung/galaxys/prebuilt/hw/sensors.SGH-T959.so:system/lib/hw/sensors.SGH-T959.so \
+    device/samsung/galaxys/prebuilt/hw/gralloc.s5pc110.so:system/lib/hw/gralloc.s5pc110.so
 
 #
 # Keys
 #
 PRODUCT_COPY_FILES += \
-    device/samsung/i9000/prebuilt/keylayout/s3c-keypad.kl:system/usr/keylayout/s3c-keypad.kl \
-    device/samsung/i9000/prebuilt/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
-    device/samsung/i9000/prebuilt/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-    device/samsung/i9000/prebuilt/keylayout/melfas-touchkey.kl:system/usr/keylayout/melfas-touchkey.kl \
-    device/samsung/i9000/prebuilt/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-    device/samsung/i9000/prebuilt/keychars/s3c-keypad.kcm.bin:system/usr/keychars/s3c-keypad.kcm.bin \
-    device/samsung/i9000/prebuilt/keychars/sec_jack.kcm.bin:system/usr/keychars/sec_jack.kcm.bin \
-    device/samsung/i9000/prebuilt/keychars/melfas-touchkey.kcm.bin:system/usr/keychars/melfas-touchkey.kcm.bin \
-    device/samsung/i9000/prebuilt/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
-    device/samsung/i9000/prebuilt/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin  
+    device/samsung/galaxys/prebuilt/keylayout/s3c-keypad.kl:system/usr/keylayout/s3c-keypad.kl \
+    device/samsung/galaxys/prebuilt/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
+    device/samsung/galaxys/prebuilt/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    device/samsung/galaxys/prebuilt/keylayout/melfas-touchkey.kl:system/usr/keylayout/melfas-touchkey.kl \
+    device/samsung/galaxys/prebuilt/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/samsung/galaxys/prebuilt/keychars/s3c-keypad.kcm.bin:system/usr/keychars/s3c-keypad.kcm.bin \
+    device/samsung/galaxys/prebuilt/keychars/sec_jack.kcm.bin:system/usr/keychars/sec_jack.kcm.bin \
+    device/samsung/galaxys/prebuilt/keychars/melfas-touchkey.kcm.bin:system/usr/keychars/melfas-touchkey.kcm.bin \
+    device/samsung/galaxys/prebuilt/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
+    device/samsung/galaxys/prebuilt/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin  
 
 
 #
 # Vold
 #
 PRODUCT_COPY_FILES += \
-    device/samsung/i9000/prebuilt/vold.fstab:system/etc/vold.fstab 
+    device/samsung/galaxys/prebuilt/vold.fstab:system/etc/vold.fstab 
 
 #
 # RIL
 #
 PRODUCT_COPY_FILES += \
-    device/samsung/i9000/prebuilt/ril/libsec-ril.so:system/lib/libsec-ril.so \
-    device/samsung/i9000/prebuilt/ril/libsecril-client.so:system/lib/libsecril-client.so \
-    device/samsung/i9000/prebuilt/ril/drexe:system/bin/drexe \
-    device/samsung/i9000/prebuilt/ril/efsd:system/bin/efsd \
-    device/samsung/i9000/prebuilt/ril/efsd30:system/bin/efsd30 \
-   device/samsung/i9000/prebuilt/ril/rilclient-test:system/bin/rilclient-test
+    device/samsung/galaxys/prebuilt/ril/libsec-ril.so:system/lib/libsec-ril.so \
+    device/samsung/galaxys/prebuilt/ril/libsecril-client.so:system/lib/libsecril-client.so \
+    device/samsung/galaxys/prebuilt/ril/drexe:system/bin/drexe \
+    device/samsung/galaxys/prebuilt/ril/efsd:system/bin/efsd \
+    device/samsung/galaxys/prebuilt/ril/efsd30:system/bin/efsd30 \
+   device/samsung/galaxys/prebuilt/ril/rilclient-test:system/bin/rilclient-test
 
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
-# Passion uses high-density artwork where available
+# GalaxyS uses high-density artwork where available
 PRODUCT_LOCALES := hdpi
 
 
